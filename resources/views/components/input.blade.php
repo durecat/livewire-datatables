940ef746839fe3b604d3904c2da3184d793239d2
durecat <durecat@gmail.com>
2021-02-02 22:49:42 -0500

    fixing inputsync error

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -3,7 +3,7 @@
 <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:pt-5">
     <label for="{{ $column['name'] }}" class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">{{ $column['label'] }}</label>
     <div class="mt-1 sm:mt-0 sm:col-span-2">
-        <input wire:loading.attr="disabled" wire:model.defer="{{ $table }}.{{ $column['name'] }}" type="{{ $column['input'] }}"
+        <input wire:loading.attr="disabled" wire:model.lazy="{{ $table }}.{{ $column['name'] }}" type="{{ $column['input'] }}"
             name="{{ $column['name'] }}" id="{{ $column['name'] }}"
             class="max-w-lg block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm">
         @error($table.".".$column['name'])

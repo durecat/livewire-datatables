0b16c6498b7f474e37c69ad87f78126e665def6b
durecat <durecat@gmail.com>
2021-02-02 22:38:06 -0500

    deleted dump

--- src/Traits/WithCreateOrEditAction.php
+++ src/Traits/WithCreateOrEditAction.php
@@ -11,7 +11,6 @@ trait WithCreateOrEditAction
 
     public function showCreateModal($id=null)
     {
-        dump('showCreateModal: '.$id.'///');
         if($id){
             $this->editingMode = "edit";
             $this->{$this->table} = $this->model::findOrFail($id);

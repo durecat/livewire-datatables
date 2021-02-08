28c53d4d7ceb2ab6b25ee3fbbbb85ac765588a99
Dure Choi <durecat@gmail.com>
2021-02-08 15:46:36 -0500

    removed max-auto from boolean column

--- resources/views/livewire/datatables/boolean.blade.php
+++ resources/views/livewire/datatables/boolean.blade.php
@@ -1,7 +1,7 @@
 <div>
 @if($value)
-    <x-icons.check-circle class="text-green-600 mx-auto" />
+    <x-icons.check-circle class="text-green-600" />
 @else
-    <x-icons.x-circle class="text-red-300 mx-auto" />
+    <x-icons.x-circle class="text-red-300" />
 @endif
 </div>

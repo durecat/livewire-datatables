07820abc5fda517ed2cc76272331627a8d11472e
Dure Choi <durecat@gmail.com>
2021-02-09 17:53:25 -0500

    form input layouts modifications

--- resources/views/components/checkbox.blade.php
+++ resources/views/components/checkbox.blade.php
@@ -1,11 +1,11 @@
 @props([ 'column' ])
 
-<div>
+<div class="flex items-center space-x-4 sm:pt-2">
 @foreach($column['options'] as $option)
     <label for="{{ $option['value'] }}" class="flex items-center">
         <input wire:loading.attr="disabled" wire:model.lazy="{{ $option['value'] }}" type="checkbox" id="{{ $option['value'] }}"
             class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
-        <div class="ml-2">
+        <div class="ml-1">
             {{ $option['label'] }}
         </div>
     </label>

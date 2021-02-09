a59f19cf16950e10811c9402eefb9d9314555be3
Dure Choi <durecat@gmail.com>
2021-02-09 13:55:42 -0500

    added boolean activity view

---
+++ resources/views/livewire/datatables/boolean-activity.blade.php
@@ -0,0 +1,9 @@
+@if($value)
+<span class="inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium bg-green-100 text-green-800">
+    Active
+</span>
+@else
+<span class="inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium bg-red-100 text-red-800">
+    Inactive
+</span>
+@endif

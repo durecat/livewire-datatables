62445075143528b860aefbc4ad0458abcff618bf
Dure Choi <durecat@gmail.com>
2021-02-09 18:05:11 -0500

    fixing checkbox error

--- resources/views/components/checkbox.blade.php
+++ resources/views/components/checkbox.blade.php
@@ -1,10 +1,10 @@
 @props([ 'column' ])
 
-<div class="flex items-center space-x-4 sm:pt-2">
+<div class="flex items-center space-x-4 pt-2">
 @foreach($column['options'] as $option)
     <label for="{{ $option['value'] }}" class="flex items-center">
-        <input wire:loading.attr="disabled" wire:model.lazy="{{ (boolean)$option['value'] }}" type="checkbox" id="{{ $option['value'] }}"
-            class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+        <input wire:loading.attr="disabled" wire:model.lazy="{{ $option['value'] }}" type="checkbox" id="{{ $option['value'] }}" value="1"
+        class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
         <div class="ml-1">
             {{ $option['label'] }}
         </div>
